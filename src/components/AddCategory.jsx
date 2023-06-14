ad97fbb0548226a23aa7faa01fe7e9d198b8c840
Manuel Requena <48953482+GManuelRequena@users.noreply.github.com>
2023-06-14 20:20:32 -0300

    Add posibility to add elementsin the array

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,5 +1,30 @@
-function AddCategory() {
-  return <div>AddCategory</div>;
-}
+import { useState } from "react";
+
+export const AddCategory = ({ setCategories }) => {
+  const [inputValue, setInputValue] = useState("One Punch");
+
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (inputValue.trim().length <= 1) return;
+    setCategories((categories) => [inputValue, ...categories]);
+    setInputValue("");
+  };
+
+  return (
+    <form onSubmit={(event) => onSubmit(event)}>
+      <input
+        type="text"
+        placeholder="Search Gif"
+        value={inputValue}
+        onChange={onInputChange}
+      />
+      ;
+    </form>
+  );
+};
 
 export default AddCategory;
