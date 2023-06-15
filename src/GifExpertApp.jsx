ba081c394be8db21d4a987a209d575d45ea55fd1
Manuel Requena <48953482+GManuelRequena@users.noreply.github.com>
2023-06-15 19:03:29 -0300

    Added new feature

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,30 +1,28 @@
 import { useState } from "react";
 import AddCategory from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
 
-  const onAddCategory = () => {
-    setCategories([...categories, "Valorant"]);
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+
+    setCategories([newCategory, ...categories]);
   };
-  console.log(categories);
 
   return (
     <>
       {/* titulo */}
       <h1>GifExpertApp</h1>
-
       {/* Input */}
-      <AddCategory setCategories={setCategories} />
-
+      <AddCategory onNewCategory={onAddCategory} />
       {/* Listado de gif */}
       <button onClick={onAddCategory}>Add</button>
-      <ol>
-        {categories.map((category) => {
-          return <li key={category}>{category}</li>;
-        })}
-      </ol>
-      {/* Gif item */}
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+      ;{/* Gif item */}
     </>
   );
 };
